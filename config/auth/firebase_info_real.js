76dc58f72bc75e312f09a7138dcf967f9aef2c2f
Vemulapalli <v.pramodkrishna@gmail.com>
2017-10-29 15:00:01 -0400

    .gitignore is now working

---
+++ config/auth/firebase_info_real.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "******************************",
+  authDomain: "**************.firebaseapp.com",
+  databaseURL: "https://*************.firebaseio.com",
+  projectId: "***************",
+  storageBucket: "",
+  messagingSenderId: "***************"
+};
---
+++ config/auth/index.js
@@ -0,0 +1,2 @@
+export * from './firebase_info';
+export * from './facebook_info';
