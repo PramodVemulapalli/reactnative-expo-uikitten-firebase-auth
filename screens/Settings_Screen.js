73176c892646389d60c5ce543d284f8e42e8f850
Vemulapalli <v.pramodkrishna@gmail.com>
2017-10-29 19:49:41 -0400

    test

--- screens/Settings_Screen.js
+++ screens/Settings_Screen.js
@@ -40,7 +40,7 @@ class Settings_Screen extends Component {
         rkType='awesome'
         style={{
           color: tintColor,
-          fontSize: 22,
+          fontSize: 24,
           marginBottom: 0,
         }}>
           {FontAwesome.cog}
